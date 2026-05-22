9b7204d7ebb2f1305b8720765fb8390c7130e5f0
irfanx <irfan.ulya@digitalbdg.ac.id>
2026-05-22 15:39:58 +0700

    fix test isi

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,120 +11,81 @@ export default function Home() {
 
             <div>
               <p className="text-sm text-gray-500">
-                Selamat Datang 👋
+                📝 Balance Notes
               </p>
-
-              <h1 className="text-2xl font-bold text-black mt-1">
-                Nabung.
-              </h1>
             </div>
 
             <div className="w-12 h-12 rounded-2xl bg-black text-white flex items-center justify-center text-lg font-semibold">
-              A
+              I
             </div>
 
           </div>
 
           {/* MAIN BALANCE */}
-          <section className="bg-black text-white rounded-[28px] p-6 shadow-[0_4px_10px_rgba(0,0,0,0.05)]">
+          <section className="bg-white border border-gray-200 text-black rounded-[28px] p-6 shadow-[0_4px_10px_rgba(0,0,0,0.05)]">
 
-            <p className="text-sm text-gray-300">
-              Total Tabungan
+            <p className="text-sm text-black-300">
+              Cash
             </p>
 
-            <h1 className="text-4xl font-bold mt-3">
-              Rp 12.500.000
+            <h1 className="text-4xl font-bold mt-1">
+              Rp 10.000
             </h1>
 
-            <div className="flex justify-between items-center mt-6">
+            <div className="w-[20px] h-[2px] my-7 bg-lime-900 rounded"></div>
 
-              <div>
-                <p className="text-xs text-gray-400">
-                  Target
-                </p>
+            
+            <p className="text-sm text-black-300">
+              Dana
+            </p>
 
-                <h2 className="text-lg font-semibold mt-1">
-                  Rp 20jt
-                </h2>
-              </div>
+            <h1 className="text-4xl font-bold mt-1">
+              Rp 10.000
+            </h1>
 
-              <div className="bg-white/10 border border-white/10 px-4 py-2 rounded-2xl backdrop-blur-md">
-                <span className="text-green-400 text-sm font-medium">
-                  +12%
-                </span>
-              </div>
-
-            </div>
+            
 
           </section>
+
 
           {/* QUICK STATS */}
           <section className="grid grid-cols-2 gap-4">
 
             {/* INCOME */}
-            <div className="bg-white border border-gray-200 rounded-[24px] p-5 shadow-[0_4px_10px_rgba(0,0,0,0.05)]">
+            <div className="bg-white items-center flex flex-col h-fit border border-gray-200 rounded-[24px] p-5 shadow-[0_4px_10px_rgba(0,0,0,0.05)]">
 
               <div className="w-10 h-10 rounded-2xl bg-green-100 flex items-center justify-center">
-                💰
+                📈
               </div>
 
-              <p className="text-sm text-gray-500 mt-4">
+              <p className="text-sm text-gray-500 mt-2">
                 Pemasukan
               </p>
-
-              <h1 className="text-xl font-bold text-black mt-1">
-                Rp 8.2jt
-              </h1>
-
             </div>
 
             {/* EXPENSE */}
-            <div className="bg-white border border-gray-200 rounded-[24px] p-5 shadow-[0_4px_10px_rgba(0,0,0,0.05)]">
+            <div className="bg-white items-center flex flex-col h-fit border border-gray-200 rounded-[24px] p-5 shadow-[0_4px_10px_rgba(0,0,0,0.05)]">
 
               <div className="w-10 h-10 rounded-2xl bg-red-100 flex items-center justify-center">
-                🧾
+                📉
               </div>
 
-              <p className="text-sm text-gray-500 mt-4">
+              <p className="text-sm text-gray-500 mt-2">
                 Pengeluaran
               </p>
-
-              <h1 className="text-xl font-bold text-black mt-1">
-                Rp 3.4jt
-              </h1>
-
             </div>
 
           </section>
 
-          {/* SAVING PROGRESS */}
-          <section className="bg-white border border-gray-200 rounded-[24px] p-5 shadow-[0_4px_10px_rgba(0,0,0,0.05)]">
 
-            <div className="flex justify-between items-center">
-
-              <h2 className="text-lg font-bold text-black">
-                Progress Nabung
-              </h2>
-
-              <span className="text-sm text-gray-500">
-                62%
-              </span>
-
-            </div>
-
-            <div className="w-full h-3 bg-gray-100 rounded-full mt-5 overflow-hidden">
-              <div className="w-[62%] h-full bg-black rounded-full"></div>
-            </div>
-
-          </section>
-
+          
           {/* TRANSACTION LIST */}
           <section className="bg-white border border-gray-200 rounded-[24px] p-5 shadow-[0_4px_10px_rgba(0,0,0,0.05)] flex-1">
 
             <div className="flex justify-between items-center mb-5">
 
               <h2 className="text-lg font-bold text-black">
-                Transaksi
+                Recent Transaction
               </h2>
 
               <button className="text-sm text-gray-500">
@@ -220,7 +181,7 @@ export default function Home() {
           {/* BOTTOM NAV */}
           <nav className="bg-white border border-gray-200 rounded-[24px] p-3 shadow-[0_4px_10px_rgba(0,0,0,0.05)] flex justify-around items-center">
 
-            <button className="bg-black text-white w-14 h-14 rounded-2xl flex items-center justify-center text-xl">
+            <button className="bg-white border border-blue-300  text-white w-14 h-14 rounded-2xl flex items-center justify-center text-xl">
               🏠
             </button>
 
