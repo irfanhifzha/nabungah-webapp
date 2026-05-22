0aadef43e6c2d43f190302f11637514c97205f86
irfanx <irfan.ulya@digitalbdg.ac.id>
2026-05-22 07:31:45 +0700

    add button link to another page

--- template-reactwindvitejs/src/pages/About.jsx
+++ template-reactwindvitejs/src/pages/About.jsx
@@ -2,8 +2,12 @@
 export default function About() {
   return (
     <>
-      <div className='m-0 p-0 flex justify-center items-center bg-red-500 h-screen'>
+      <div className='m-0 p-0 flex justify-center items-center bg-red-500 h-screen gap-3'>
         <div className='text-white text-sm'>Halo this is about page</div>
+
+        <button className='bg-orange-500 rounded-md p-2 cursor-pointer' onClick={() => window.location.href = "/"}>to homepage</button>
+
+        <button className='bg-orange-500 rounded-md p-2 cursor-pointer' onClick={() => window.location.href = "/notfounasdf"}>to 404 page</button>
 
       </div>
 
--- template-reactwindvitejs/src/pages/Home.jsx
+++ template-reactwindvitejs/src/pages/Home.jsx
@@ -3,8 +3,10 @@ export default function Home() {
 
   return (
     <>
-      <div className='m-0 p-0 flex justify-center items-center bg-red-500 h-screen'>
+      <div className='m-0 p-0 flex justify-center items-center flex flex-col gap-4 bg-emerald-500 h-screen'>
         <div className='text-white text-sm'>Halo</div>
+
+        <button className='bg-orange-500 rounded-md p-2 cursor-pointer' onClick={() => window.location.href = "/about"}>to about</button>
 
       </div>
 
